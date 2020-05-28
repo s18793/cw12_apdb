401b728fb7616a3dfa875a316fb6ac1b97f94a07
Paweł Pietrzak <s18793@pjwstk.edu.pl>
2020-05-28 10:15:51 +0200

    Dodaj pliki projektów.

---
+++ cw12_apdb/Controllers/DoctorsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw12_apdb.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetDoctors()
+        {
+            return View();
+        }
+    }
+}
---
+++ cw12_apdb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cw12_apdb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
